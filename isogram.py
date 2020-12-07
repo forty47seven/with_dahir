a921642b8f3d2813f80c9826258ecc08129372ba
forty47seven <58090281+forty47seven@users.noreply.github.com>
2020-12-07 23:52:26 +0100

    Add files via upload
    corrected

--- isogram.py
+++ isogram.py
@@ -1,11 +1,14 @@
 text = input('Enter a word: ')
 count = 0
+
 for i in range(len(text)):
-    for j in range(len(text) - 1):
-        if count < 1 and i != j:
-            if text[i] == text[j]:
-                count += 1
-                print ('The word is not an isogram')
-        elif count < 1:
+
+    for j in range(len(text) - (1 + i)):
+
+        if count < 1 and text[i] == text[j + (i + 1)]:
+            print (text + ' is not an isogram')
+            
             count += 1
-            print ('The word is an isogram')
+        
+if count == 0:
+    print (text + ' is an isogram')
