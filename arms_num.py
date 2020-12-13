a24dbfe116b29acdae147763d97198c3a4e6ecec
forty47seven <58090281+forty47seven@users.noreply.github.com>
2020-12-13 09:43:28 +0100

    Add files via upload

---
+++ arms_num.py
@@ -0,0 +1,10 @@
+num = input('Enter a number to test: ')
+sum = 0
+
+for i in range(len(num)):
+    sum = sum + int(num[i])**len(num)
+
+if sum == int(num):
+    print (num, 'is an Armstrong number')
+else:
+    print (num, 'is not an Armstrong number')
