4b4df3f32f64cb5fbfb9e0641610798b3f011e20
forty47seven <58090281+forty47seven@users.noreply.github.com>
2020-12-13 09:20:33 +0100

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,15 @@
+word = input('Emter main word: ')
+cand = input('Enter candidate, separated with spaces: ')
+l_cand = cand.split()
+
+for i in l_cand:
+    check = 0
+    for j in i:
+        count = 1
+        for k in word:
+            if j == k:
+                count += 1
+                if count > 1:
+                    check += 1
+    if check == len(word):
+        print (i)
