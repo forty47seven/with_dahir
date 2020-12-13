326d269f10621a34a2aca3065439cd81883c1528
forty47seven <58090281+forty47seven@users.noreply.github.com>
2020-12-13 07:46:38 +0100

    Add files via upload

---
+++ RLE.py
@@ -0,0 +1,27 @@
+text = input('Enter a text: ')
+
+pos = 0
+nxt_pos = 1
+comp = ''
+
+while pos < len(text):
+    count = 1
+    while text[pos] == text[nxt_pos] and pos != nxt_pos:
+        count += 1
+        pos += 1
+        nxt_pos += 1
+
+        if nxt_pos == len(text):
+            nxt_pos = pos
+
+    if count > 1:
+        comp = comp + str(count) + text[pos]
+    else:
+        comp = comp + text[pos]
+    pos = pos + 1
+    nxt_pos = nxt_pos + 1
+    
+    if nxt_pos == len(text):
+        nxt_pos = pos
+
+print (comp)
